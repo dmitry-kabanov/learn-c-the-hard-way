cf91c0f6174be53d386c5606f5a3049a6dafc83d
Dmitry Kabanov <kabanovdmitry@gmail.com>
2013-01-03 20:16:43 +0400

    Initial commit.

---
+++ Makefile
@@ -0,0 +1,8 @@
+CFLAGS=-Wall -O0 -g
+
+all: ex1 ex3
+
+clean:
+	rm -f ex1 ex3
+	rm -rf *dSYM
+
---
+++ ex1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    puts("Hello World!");
+    puts("My name is Dmitry.");
+
+    return 0;
+}
