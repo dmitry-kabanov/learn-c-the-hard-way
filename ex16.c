bc34fecfc6149f55c3e2667f99592fc4496872c3
Dmitry Kabanov <kabanovdmitry@gmail.com>
2013-01-20 12:58:05 +0000

    add exercise 16 program

--- ex16.c
+++ ex16.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
 
 struct Person {
     char *name;
@@ -7,12 +10,12 @@ struct Person {
     int weight;
 };
 
-struct Person *Person_create(char *name, int age, int height, int weight)
+struct Person *Person_create(const char *name, int age, int height, int weight)
 {
     struct Person *who = malloc(sizeof(struct Person));
     assert(who != NULL);
 
-    who->name = name;
+    who->name = strdup(name);
     who->age = age;
     who->height = height;
     who->weight = weight;
@@ -20,7 +23,7 @@ struct Person *Person_create(char *name, int age, int height, int weight)
     return who;
 }
 
-vooid Person_destroy(struct Person *who)
+void Person_destroy(struct Person *who)
 {
     assert(who != NULL);
     free(who);
@@ -28,7 +31,7 @@ vooid Person_destroy(struct Person *who)
 
 void Person_print(struct Person *who)
 {
-    printf("Name :%s\n", who->name);
+    printf("Name:   %s\n", who->name);
     printf("\tAge: %d\n", who->age);
     printf("\tHeight: %d\n", who->height);
     printf("\tWeight: %d\n", who->weight);
@@ -36,5 +39,27 @@ void Person_print(struct Person *who)
 
 int main(int argc, char *argv[])
 {
-}
+    struct Person *joe = Person_create("Joe Alex", 32, 64, 140);
+    struct Person *frank = Person_create("Frank Blank", 20, 72, 180);
 
+    printf("Joe is at memory location: %p\n", joe);
+    Person_print(joe);
+
+    printf("Frank is at memory location: %p\n", frank);
+    Person_print(frank);
+
+    joe->age += 20;
+    joe->height += 2;
+    joe->weight += 40;
+    Person_print(joe);
+
+    frank->age += 20;
+    frank->height += 0;
+    frank->weight += 20;
+    Person_print(frank);
+
+    Person_destroy(joe);
+    Person_destroy(frank);
+
+    return 0;
+}
