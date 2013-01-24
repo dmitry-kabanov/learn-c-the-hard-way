af060ad84d60a081663ddc2f97d90e2db6546623
Dmitry Kabanov <kabanovdmitry@gmail.com>
2013-01-24 17:14:21 +0300

    fix an error with setting name and email properties in the case when their lengthes exceed maximum possible value

--- ex17.c
+++ ex17.c
@@ -127,14 +127,18 @@ void Database_set(struct Connection *conn, int id, const char *name, const char 
     }
 
     addr->set = 1;
-    // WARNING: bug, read the "How to Break It" and fix this
     char *res = strncpy(addr->name, name, MAX_DATA);
-    // demonstrate the strncpy bug
+    if (strlen(name) >= MAX_DATA) {
+        addr->name[MAX_DATA - 1] = '\0';
+    }
     if (!res) {
         die("Name copy failed.");
     }
 
     res = strncpy(addr->email, email, MAX_DATA);
+    if (strlen(email) >= MAX_DATA) {
+        addr->email[MAX_DATA - 1] = '\0';
+    }
     if (!res) {
         die("Email copy failed.");
     }
